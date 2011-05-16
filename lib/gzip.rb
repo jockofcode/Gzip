fc1b4e573848d84f92650f5e52030329b7b118fd
JockOfCode <branden@carbondatacomputers.com>
2011-05-16 13:30:23 -0600

    Here it is\!

---
+++ lib/gzip.rb
@@ -0,0 +1,28 @@
+require 'zlib'
+require 'stringio'
+
+class String
+	def gzip
+		if self.length > 0 then
+			zdata = "";sio = StringIO.new(zdata)
+			gz = Zlib::GzipWriter.new(sio)
+			gz.write(self)
+			gz.close
+			zdata
+		else
+			""
+		end
+	end
+	def gunzip
+		if self.length > 0 then
+			sio = StringIO.new(self)
+			gz = Zlib::GzipReader.new(sio)
+			string = gz.read
+			gz.close
+			string
+		else
+			""
+		end
+	end
+end
+
